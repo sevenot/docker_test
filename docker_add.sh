9411ed3404f4e5d9defd9874eef40c2dba114b77
sevenot <sevenot@126.com>
2017-08-19 23:05:55 +0800

    init

---
+++ docker_add.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo -e "\033[32mThis scritps will create container!\033[0m"
+read -p "Please input the number of containers you want to create : " -t 15 num_con
+read -p "Please input the init name for container : " -t 15  name_con
+read -p "Please input the first host port number to transfor the container's port : " -t 15 port
+for((i=1;i<=$num_con;i=i+1))
+	do
+		_port=$(($port+i-1))
+		docker run -t -i --name "$name_con$i" -d -p $_port:80 ubuntu:14.04 /bin/bash
+	done
